f2ee53a081514c7ee9408b1c1a36983f8e8d593c
artem shutak <artem.shutak@gmail.com>
2011-04-30 13:57:59 +0400

    Save RandomMatrix in file

--- src/com/shutart/rpkdtree/rpkdtrees/RPKDTrees.java
+++ src/com/shutart/rpkdtree/rpkdtrees/RPKDTrees.java
@@ -1,5 +1,6 @@
 package com.shutart.rpkdtree.rpkdtrees;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,7 @@ import com.shutart.rpkdtree.kdtree.INode;
 import com.shutart.rpkdtree.kdtree.KDTree;
 import com.shutart.rpkdtree.kdtree.Node;
 import com.shutart.rpkdtree.kdtree.Vector;
+import com.shutart.rpkdtree.kdtree.VectorI;
 
 /**
  * Look at paper "Randomly Projected KD-Trees 
@@ -23,7 +25,7 @@ public class RPKDTrees {
 	private final int myProjectedDimension;
 	private final KDTree[] myProjTrees;
 	private final RandomMatrix[] myRandomMatrixes;
-	private final Map<Vector, Vector> projVec2SourceVec = new HashMap<Vector, Vector>();
+	private final Map<VectorAndSpaceNum, Vector> projVec2SourceVec = new HashMap<VectorAndSpaceNum, Vector>();
 
 	public RPKDTrees(int dimension, int projectidDimension,int numberOfTrees){
 		assert dimension > projectidDimension;
@@ -31,10 +33,10 @@ public class RPKDTrees {
 		this.myProjectedDimension = projectidDimension;
 		myProjTrees = new KDTree[numberOfTrees];
 		
-		myRandomMatrixes = new RandomMatrix[numberOfTrees];
+		myRandomMatrixes = RandomMatrix.getMatrixes(dimension,projectidDimension,numberOfTrees);
 		for (int i = 0; i < numberOfTrees; i++) {
 			myProjTrees[i] = new KDTree(myProjectedDimension);
-			myRandomMatrixes[i] = new RandomMatrix(myDimension, myProjectedDimension);
+			//myRandomMatrixes[i] = new RandomMatrix(myDimension, myProjectedDimension);
 		}
 	}
 	
@@ -46,10 +48,10 @@ public class RPKDTrees {
 	public void indexing(Set<Vector> corpus) {
 		for (Vector vector : corpus) {
 			assert vector.size() == myDimension;
-			for (int i = 0; i < getNumberOfTrees(); i++) {
-				Vector projVec = myRandomMatrixes[i].multiply(vector);
-				projVec2SourceVec.put(projVec, vector);
-				myProjTrees[i].insert(projVec );
+			for (int spaceNum = 0; spaceNum < getNumberOfTrees(); spaceNum++) {
+				Vector projVec = myRandomMatrixes[spaceNum].multiply(vector);
+				projVec2SourceVec.put(new VectorAndSpaceNum(projVec, spaceNum), vector);
+				myProjTrees[spaceNum].insert(projVec );
 			}
 		}
 	}
@@ -61,11 +63,12 @@ public class RPKDTrees {
 	public List<Vector> aproxNNsearch (int numberOfNeighbors, Vector queryVector){
 		assert queryVector.size() == myDimension;
 		FixedSizeVecPriorQueue vecQueue = new MyFixedSizeVecPriorQueue(numberOfNeighbors, queryVector);
-		for (int i = 0; i < getNumberOfTrees(); i++) {
+		for (int spaceNum = 0; spaceNum < getNumberOfTrees(); spaceNum++) {
 			List<Vector> nearestNeighbors = 
-				myProjTrees[i].nnsearch(numberOfNeighbors, myRandomMatrixes[i].multiply(queryVector));
+				myProjTrees[spaceNum].nnsearch(numberOfNeighbors, myRandomMatrixes[spaceNum].multiply(queryVector));
 			for (Vector vector : nearestNeighbors) {
-				vecQueue.add(projVec2SourceVec.get(vector));
+				Vector newVector = projVec2SourceVec.get(new VectorAndSpaceNum(vector, spaceNum));
+				vecQueue.add(newVector);
 			}
 		}
 		return vecQueue.getNearestNeighbors();
@@ -82,6 +85,35 @@ public class RPKDTrees {
 			if(!this.contains(vector)){
 				super.add(vector);
 			}
+		}
+		
+	}
+	
+	private class VectorAndSpaceNum{
+
+		private final int spaceNum;
+		private final Vector vec;
+
+		public VectorAndSpaceNum(Vector projVec, int spaceNum){
+			this.vec = projVec;
+			this.spaceNum = spaceNum;
+		}
+
+		public int getSpaceNum() {
+			return spaceNum;
+		}
+
+		public Vector getVector() {
+			return vec;
+		}
+		
+		@Override
+		public boolean equals(Object vectorAndNum) {
+			if(! (vectorAndNum instanceof VectorAndSpaceNum)){
+				throw new IllegalArgumentException();
+			}
+			return spaceNum==((VectorAndSpaceNum)vectorAndNum).spaceNum &&
+					vec.equals(((VectorAndSpaceNum)vectorAndNum).vec);
 		}
 		
 	}
--- src/com/shutart/rpkdtree/rpkdtrees/RandomMatrix.java
+++ src/com/shutart/rpkdtree/rpkdtrees/RandomMatrix.java
@@ -1,6 +1,15 @@
 package com.shutart.rpkdtree.rpkdtrees;
 
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Random;
+import java.util.Scanner;
 
 import com.shutart.rpkdtree.kdtree.INode;
 import com.shutart.rpkdtree.kdtree.Node;
@@ -19,8 +28,11 @@ public class RandomMatrix {
 		while(myMatrixIsZero(d,d1)){
 			
 		}
-		System.out.println(this);
-		
+		//System.out.println(this);
+	}
+
+	public RandomMatrix(int[][] matr) {
+		myMatrix = matr;
 	}
 
 	private boolean myMatrixIsZero(int d, int d1) {
@@ -74,11 +86,66 @@ public class RandomMatrix {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < getProjDimen(); i++) {
 			for (int j = 0; j < getDimension(); j++) {
-				sb.append(myMatrix[i][j]+" ");
+				sb.append(myMatrix[i][j] + " ");
 			}
 			sb.append("\n");
 		}
 		return sb.toString();
+	}
+
+	public static RandomMatrix[] getMatrixes(int dimension,
+			int projectidDimension, int numberOfTrees) {
+		String matrixesFileName = "matrixes" + "_d="+dimension+
+											   "_d1=" + projectidDimension+
+											   "_m=" + numberOfTrees+
+											   ".txt";
+		File thisMatrFile = new File("matrixes\\" + matrixesFileName );
+		if(!thisMatrFile.exists()){
+			return createMatrixesFile(thisMatrFile, dimension, projectidDimension,numberOfTrees);
+		}
+		return readMatrixesFromFile(thisMatrFile, dimension,projectidDimension, numberOfTrees);
+	}
+
+	private static RandomMatrix[] readMatrixesFromFile(File matrFile,
+			int dimension, int projectidDimension, int numberOfTrees) {
+		try {
+			Scanner sc = new Scanner(matrFile);
+			RandomMatrix[] resRandMatrs = new RandomMatrix[numberOfTrees];
+			while(sc.hasNextInt()){
+				for (int numOfMatr = 0; numOfMatr < resRandMatrs.length; numOfMatr++) {
+					int[][] newMatr = new int[projectidDimension][dimension];
+					for (int i = 0; i < projectidDimension; i++) {
+						for (int j = 0; j < dimension; j++) {
+							newMatr[i][j] = sc.nextInt();
+						}
+					}
+					resRandMatrs[numOfMatr] = new RandomMatrix(newMatr);					
+				}
+			}
+			return resRandMatrs;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static RandomMatrix[] createMatrixesFile(File fileName, int dimension,
+			int projectidDimension, int numberOfTrees) {
+		try {
+			RandomMatrix[] resRandMatrs = new RandomMatrix[numberOfTrees];
+			Writer output = new BufferedWriter(new FileWriter(fileName));
+			for (int i = 0; i < resRandMatrs.length; i++) {
+				RandomMatrix newMatr = new RandomMatrix(dimension, projectidDimension);
+				output.write(newMatr.toString()+"\n\n");
+				resRandMatrs[i] = newMatr;					
+			}
+			output.close();
+			return resRandMatrs;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
